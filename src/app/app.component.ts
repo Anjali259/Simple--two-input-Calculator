33ae8249bb6cecac7460d3d4eee772b3b640de89
Anjali <Anjali259@users.noreply.github.com>
2019-08-01 16:25:22 +0530

    Calculator Initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+  num1: number;
+  num2: number;
+  public result: number;
+  public displayname: boolean;
+
+  onAddition() {
+    this.displayname = true;
+    this.result = this.num1 + this.num2;
+
+  }
+
+  onSubtraction() {
+    this.displayname = true;
+    this.result = this.num1 - this.num2;
+  }
+  onDivision() {
+    this.displayname = true;
+    this.result = this.num1 / this.num2;
+  }
+
+  onMultiplication() {
+    this.displayname = true;
+    this.result = this.num1 * this.num2;
+  }
+  clear() {
+    this.displayname = false;
+    this.num1 = this.num2 = null;
+  }
 }
