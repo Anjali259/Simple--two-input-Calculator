24968db02f713266cc1f62f9742dea7cc950ee5a
Anjali <Anjali259@users.noreply.github.com>
2019-08-05 18:43:09 +0530

    WIP - added combination function of given input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,10 @@ export class AppComponent {
   num1: number;
   num2: number;
   public result: number;
+  public combineresult: number;
   public displayname: boolean;
   public displayreverse: boolean;
+  displaycombine: boolean;
 
   onAddition() {
     this.displayname = true;
@@ -38,21 +40,58 @@ export class AppComponent {
     this.displayreverse = false;
     this.num1 = this.num2 = null;
   }
- 
+
   reverse(num) { // number = 2345
     this.displayreverse = true;
     let str = num.toString(); // converted number to string
     let r = '';
-    for (var i = str.length - 1; i >= 0; i--)  
+    for (var i = str.length - 1; i >= 0; i--)
     // Initially i will be 3 (string length is 4 (number is 2345) - 1) 
     {
       r = r + str[i]; //so str[i] = str[3] = 5 from (number = 2345) reverse
       console.log(r);
     }
     return this.result = parseInt(r);
+  }
 
+  combinenum(str) {
+   
+    let num = str.toString();
+    let result = [];
+    let r1 = '';
+    let r2 = '';
+    let r3: number;
+    let r4: number;
+    this.displaycombine = true;
+    // Since you only want pairs, there's no reason
+    // to iterate over the last element directly
+    for (let i = 0; i < num.length - 1; i++) {
+      // This is where you'll capture that last value
+      for (let j = i + 1; j < num.length; j++) {
+        r1 = num[i] + num[j];
+        r2 = num[j] + num[i];
+        r3 = parseInt(r1);
+        r4 = parseInt(r2);
+        result.push(r3);
+        result.push(r4);
+          
+         }
+    }
+    alert(result);
+    
+    
   }
 
 }
+
+
+
+
+
+
+
+
+
+
 
 
